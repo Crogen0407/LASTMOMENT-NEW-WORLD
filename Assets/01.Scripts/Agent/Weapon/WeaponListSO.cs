f55e6313fdca2cd8bd4b8c6ac8b4256ae9e05ace
Crogen0407 <mymcoding0407@gmail.com>
2024-05-26 01:54:57 +0900

    feat : WeaponSO

---
+++ Assets/01.Scripts/Agent/Weapon/WeaponListSO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/WeaponList")]
+public class WeaponListSO : ScriptableObject
+{
+    public WeaponSO[] weapons;
+
+    [ContextMenu("ResetIndex")]
+    private void ResetIndex()
+    {
+        if (weapons != null)
+        {
+            for (int i = 0; i < weapons.Length; ++i)
+            {
+                if(weapons[i]!=null)
+                    weapons[i].index = i;
+            }
+        }
+    }
+}
