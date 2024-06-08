c4589cf64763a8ec674d15057d8232bce67cd297
Crogen0407 <mymcoding0407@gmail.com>
2024-06-08 14:18:08 +0900

    feat : Minimap Clamp

--- Assets/01.Scripts/MapPointSign.cs
+++ Assets/01.Scripts/MapPointSign.cs
@@ -1,8 +1,17 @@
-using System;
 using UnityEngine;
 
 public class MapPointSign : MonoBehaviour
 {
+    private Transform _radarCam;
+    private float _camSize;
+    [SerializeField] private Transform _mapEnablePoint;
+
+    private void Awake()
+    {
+        _radarCam = GameObject.Find("RadarCamera").transform;
+        _camSize = _radarCam.GetComponent<Camera>().orthographicSize-50;
+    }
+
     private void OnValidate()
     {
         if (!transform.parent) return;
@@ -15,5 +24,18 @@ public class MapPointSign : MonoBehaviour
     private void FixedUpdate()
     {
         transform.forward = -Vector3.up;
+
+        Vector3 camPos = new Vector3(_radarCam.position.x, _mapEnablePoint.position.y, _radarCam.position.z);
+        Vector3 selfPos =  _mapEnablePoint.position;
+        
+        if (Vector3.Distance(selfPos, camPos) > _camSize)
+        {
+            selfPos = ((selfPos - camPos).normalized * _camSize) + camPos;
+        }
+        else
+        {
+            selfPos = transform.position;
+        }
+        _mapEnablePoint.position = selfPos;
     }
 }
