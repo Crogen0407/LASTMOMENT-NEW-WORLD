320378b106ab51942a2c895b58190659cc903235
Crogen0407 <mymcoding0407@gmail.com>
2024-06-16 04:00:26 +0900

    feat : Boss Movement

--- Assets/01.Scripts/Agent/Boss/BossMovement.cs
+++ Assets/01.Scripts/Agent/Boss/BossMovement.cs
@@ -1,7 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BossMovement : AgentMovement
 {
+    private Transform _playerTrm;
+    [SerializeField] private float _rotateSpeed = 5f;
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _playerTrm = GameManager.Instance.Player.transform;
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        Vector3 newPos = new Vector3(_playerTrm.position.x, transform.position.y, _playerTrm.position.z);
+        Quaternion lookRot = Quaternion.LookRotation(newPos - transform.position);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRot, _rotateSpeed * (1/Quaternion.Angle(transform.rotation, lookRot)) * Time.fixedDeltaTime);
+
+        CurSpeed = MaxSpeed;
+    }
+
     public override void HandleMoveDirection(Vector3 Delta)
     {
         
