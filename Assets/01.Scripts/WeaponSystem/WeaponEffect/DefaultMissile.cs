50c41cd632918a3fc3886dff54918bd53826aa8e
Crogen0407 <mymcoding0407@gmail.com>
2024-08-05 19:26:22 +0900

    TGA Weapon Effect

--- Assets/01.Scripts/WeaponSystem/WeaponEffect/DefaultMissile.cs
+++ Assets/01.Scripts/WeaponSystem/WeaponEffect/DefaultMissile.cs
@@ -1,18 +1,51 @@
+using Crogen.ObjectPooling;
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DefaultMissile : WeaponEffect
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private PoolType _explosionEffectPoolType;
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _damaged = 10f;
+    private Vector3 _targetPos;
+
+    public override void Init(Vector3 attackDirection, Transform parent = null)
     {
-        
+        base.Init(attackDirection, null);
+        transform.DOLocalMoveY(transform.position.y - 1, 1);
+        SoundManager.Instance.PlaySFX(_fireAudioType, transform.position);
+        _targetPos = transform.position + transform.forward * duration * _speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Physics.OverlapSphereNonAlloc(transform.position, attackRange, _attackTargets, _whatIsEnemy);
+
+        transform.DOMove(_targetPos, duration/_speed);
+
+        if (duration < _curLifeTime)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _curLifeTime += Time.deltaTime;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SoundManager.Instance.PlaySFX(_attackAudioType, transform.position);
+        this.Pop(_explosionEffectPoolType, transform.position, Quaternion.identity);
+        transform.DOKill();
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * _speed);
+        Gizmos.color = Color.white;
     }
 }
