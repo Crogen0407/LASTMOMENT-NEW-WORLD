5efb274133416fbd366a090fe1b6d8606b5da7ad
Crogen0407 <mymcoding0407@gmail.com>
2025-05-06 20:16:42 +0900

    delete : ACMData

--- Assets/01.Scripts/ACM/ACMDataListSO.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SO/ACMDataList")]
-public class ACMDataListSO : ScriptableObject
-{
-    public List<ACMDataSO> list;
-}
